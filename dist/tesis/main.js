51460dee861fe7d4999d231e511f789ca8fcb57f
Lui <luisangelnav12@gmail.com>
2021-06-29 19:42:27 -0500

    er

--- dist/tesis/main.js
+++ dist/tesis/main.js
@@ -3007,14 +3007,16 @@ const admin = [
     // { id: 0, name: 'Inicio' },
     // { id: 1, name: 'Usuario' },
     { id: 2, name: 'Clientes' },
-    { id: 4, name: 'Predicción' },
+    { id: 3, name: 'Ventas' },
     { id: 7, name: 'Producto' },
     { id: 8, name: 'Cerrar Sesión' },
 ];
 const client = [
-    { id: 0, name: 'Inicio' },
     { id: 3, name: 'Ventas' },
+    { id: 4, name: 'Predicción' },
+    { id: 5, name: 'Reportes' },
     { id: 6, name: 'Orden de compra' },
+    { id: 7, name: 'Producto' },
     { id: 8, name: 'Cerrar Sesión' },
 ];
 
